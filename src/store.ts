c5a0572a73ca24fde972858f2796b2e4018bd331
bherbruck <brennen.herbruck@gmail.com>
2021-05-12 15:30:13 -0400

    Create store.ts

---
+++ src/store.ts
@@ -0,0 +1,19 @@
+import * as Store from 'electron-store'
+import { watchFile } from 'fs'
+import { register } from './actions'
+
+export const store = new Store()
+
+export const defaultKeys = [162, 91]
+
+export function create() {
+  const keyCodeReference = `https://github.com/bherbruck/push-to-talk/blob/main/docs/key-codes.md`
+  store.set('keyCodeReference', keyCodeReference)
+  store.set('shortcut', store.get('shortcut') || defaultKeys)
+  register(store.get('shortcut') || defaultKeys)
+
+  watchFile(store.path, (curr, prev) => {
+    console.log('updated')
+    register(store.get('shortcut') as number[])
+  })
+}
